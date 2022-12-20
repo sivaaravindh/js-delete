bf0c53d9ee52b2eff9b62dc2d3c71d8361033ca4
sivaaravindh <sivaaravind262003@gmail.com>
2022-12-20 02:47:07 -0800

    index

---
+++ main.js
@@ -0,0 +1,30 @@
+let fruit=["apple","orange","mango","bannana","lemon"];
+let delArray=[];
+
+let b=0;
+let a =parseInt(prompt("enter the name"));
+let j;
+fruit[a]=j;
+for(i=0;i<fruit.length; i++){
+
+  if(fruit[i] != j){
+    delArray[b]=fruit[i]
+    b++;
+  }
+}
+
+
+console.log(delArray)
+
+
+/*for(i=0; i<=a; i++){
+
+  fruit[i]=c
+      
+      
+}*/
+
+
+
+
+
